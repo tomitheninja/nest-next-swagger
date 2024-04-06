8302055935ebf4a3ed32b3ef844e43c719e50710
Tamas Sudi <57364372+tomitheninja@users.noreply.github.com>
2024-04-06 22:20:26 +0200

    Fix vercel api url

--- src/my-client-api.ts
+++ src/my-client-api.ts
@@ -3,7 +3,8 @@ import { Configuration, DefaultApi } from "../generated/my-client";
 export const myClientApi = new DefaultApi(
   new Configuration({
     basePath:
-      process.env.NEXT_PUBLIC_API_URL ??
-      `http://localhost:${process.env.PORT ?? "3300"}`,
+      process.env.NEXT_PUBLIC_API_URL ?? process.env.PORT
+        ? `http://localhost:${process.env.PORT}`
+        : undefined,
   })
 );
