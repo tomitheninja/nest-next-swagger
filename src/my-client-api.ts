2dcc7eac570567f9ab8234430d3d934af4e41c76
Tamas Sudi <57364372+tomitheninja@users.noreply.github.com>
2024-04-06 22:44:06 +0200

    use vercel url

--- src/my-client-api.ts
+++ src/my-client-api.ts
@@ -1,3 +1,10 @@
-import { DefaultApi } from "../generated/my-client";
+import { Configuration, DefaultApi } from "../generated/my-client";
 
-export const myClientApi = new DefaultApi();
+export const myClientApi = new DefaultApi(
+  new Configuration({
+    basePath:
+      process.env.NEXT_PUBLIC_API_URL ??
+      process.env.NEXT_PUBLIC_VERCEL_URL ??
+      `http://localhost:${process.env.PORT ?? "3300"}`,
+  })
+);
