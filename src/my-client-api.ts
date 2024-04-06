ce7c701c2254743f6f7503a08fd9d97afb31d324
Tamas Sudi <57364372+tomitheninja@users.noreply.github.com>
2024-04-06 22:25:40 +0200

    empty config

--- src/my-client-api.ts
+++ src/my-client-api.ts
@@ -1,10 +1,3 @@
-import { Configuration, DefaultApi } from "../generated/my-client";
+import { DefaultApi } from "../generated/my-client";
 
-export const myClientApi = new DefaultApi(
-  new Configuration({
-    basePath:
-      process.env.NEXT_PUBLIC_API_URL ?? process.env.PORT
-        ? `http://localhost:${process.env.PORT}`
-        : undefined,
-  })
-);
+export const myClientApi = new DefaultApi();
